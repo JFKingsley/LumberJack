d0049cdf80c450e2f72c2795e9f405ce006cc0d4
Jonathan Kingsley <me@jfkingsley.co.uk>
2014-07-07 23:10:22 +0100

    Updated tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var LumberJack = require('../main.js');
 
-var lumberjack = new LumberJack('https://bfaa14e07f664ae2bc4bdb48f0f7e485:a065759119024a8cbcb91d336a24703c@app.getsentry.com/26633', undefined, {logFile: './testLogfile.log', prefix: 'Lumberjack', timestamp: false, colors: true});
+var lumberjack = new LumberJack('{{ Sentry DSN }}', undefined, {logFile: './testLogfile.log', prefix: 'Lumberjack', timestamp: false, colors: true});
 
 lumberjack.info('This is a info log');
 
